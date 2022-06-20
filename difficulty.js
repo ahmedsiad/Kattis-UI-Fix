485b86d130b09adcee39e9e030b479224a70f804
Hazem Ammar <45862155+Hazem-A@users.noreply.github.com>
2022-06-20 15:51:37 -0600

    Update difficulty.js

--- difficulty.js
+++ difficulty.js
@@ -1,6 +1,7 @@
 
 // add the difficulty back into kattis
 
+
 let container = document.getElementsByClassName("attribute_list-book")[0]
 
 let diff = document.createElement("div")
@@ -9,4 +10,21 @@ let s = document.createElement("span")
 s.className="attribute_list-label"
 s.innerHTML="Difficulty"
 diff.appendChild(s)
+
+
+// use binary search to get the problem difficulty
+const prob_name = document.querySelectorAll(".book-page-heading")[0]
+let low = 0
+let high = 34
+let mid  = math.floor((low+high)/2)
+
+let url = "https://open.kattis.com/problems?page="
+
+// check to see if the problem is on the page
+let req = await fetch("https://open.kattis.com/problems?page=0");
+let raw = await req.text();
+let page = new DOMParser().parseFromString(raw, "text/html");
+console.log(page)
+let probs = page.querySelectorAll(".difficulty_number")
+console.log(probs)
 container.appendChild(diff)
