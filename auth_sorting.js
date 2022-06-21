932dda1806d6251757a5c7744cd8dce984fb484c
Hazem Ammar <45862155+Hazem-A@users.noreply.github.com>
2022-06-21 16:29:20 -0600

    Add files via upload

--- auth_sorting.js
+++ auth_sorting.js
@@ -2,30 +2,58 @@ let heading = document.querySelectorAll("body > main > div.l-report_grid > artic
 
 // add three new categories to sort the problems by
 
-let auth_tot = document.querySelectorAll("body > main > div.l-report_grid > article > div > div.table-container > div > table > thead > tr > th:nth-child(8)")[0]
-let auth_tot_a = document.createElement("a")
-auth_tot_a.innerHTML="Authors"
+
+// check to see if the user is logged in
+let logged_in = document.querySelector("body > main > div.l-report_grid > article > div > div.table-container > div > table > thead > tr > th:nth-child(2) > a")
+if (logged_in.innerHTML==="Solved"){
+    let auth_tot = document.querySelector("body > main > div.l-report_grid > article > div > div.table-container > div > table > thead > tr > th:nth-child(8)")
+    let auth_tot_a = document.createElement("a")
+    auth_tot_a.innerHTML="Authors"
 // check to see if the tab is already sorting
-if (window.location.href.includes("order=-authtot")){
-    auth_tot_a.href="?order=%2Bauthtot"
-} else {
-    auth_tot_a.href="?order=-authtot"
-}
+    if (window.location.href.includes("order=-authtot")){
+        auth_tot_a.href="?order=%2Bauthtot"
+    } else {
+        auth_tot_a.href="?order=-authtot"
+    }
 
-auth_tot.appendChild(auth_tot_a)
+    auth_tot.appendChild(auth_tot_a)
 
 
-let auth_acc = document.querySelectorAll("body > main > div.l-report_grid > article > div > div.table-container > div > table > thead > tr > th:nth-child(9)")[0]
-let auth_acc_a = document.createElement("a")
-auth_acc_a.innerHTML="Auth Acc."
+    let auth_acc = document.querySelector("body > main > div.l-report_grid > article > div > div.table-container > div > table > thead > tr > th:nth-child(9)")
+    let auth_acc_a = document.createElement("a")
+    auth_acc_a.innerHTML="Auth Acc."
 // check to see if the tab is already sorting
-if (window.location.href.includes("order=-authacc")){
-    auth_acc_a.href="?order=%2Bauthacc"
+    if (window.location.href.includes("order=-authacc")){
+        auth_acc_a.href="?order=%2Bauthacc"
+    } else {
+        auth_acc_a.href="?order=-authacc"
+    }
+    auth_acc.appendChild(auth_acc_a)
 } else {
-    auth_acc_a.href="?order=-authacc"
-}
-auth_acc.appendChild(auth_acc_a)
+    let auth_tot = document.querySelector("body > main > div.l-report_grid > article > div > div.table-container > div > table > thead > tr > th:nth-child(7)")
+    let auth_tot_a = document.createElement("a")
+    auth_tot_a.innerHTML="Authors"
+// check to see if the tab is already sorting
+    if (window.location.href.includes("order=-authtot")){
+        auth_tot_a.href="?order=%2Bauthtot"
+    } else {
+        auth_tot_a.href="?order=-authtot"
+    }
 
+    auth_tot.appendChild(auth_tot_a)
+
+
+    let auth_acc = document.querySelector("body > main > div.l-report_grid > article > div > div.table-container > div > table > thead > tr > th:nth-child(8)")
+    let auth_acc_a = document.createElement("a")
+    auth_acc_a.innerHTML="Auth Acc."
+// check to see if the tab is already sorting
+    if (window.location.href.includes("order=-authacc")){
+        auth_acc_a.href="?order=%2Bauthacc"
+    } else {
+        auth_acc_a.href="?order=-authacc"
+    }
+    auth_acc.appendChild(auth_acc_a)
+}
 
 
 let auth_rat = document.createElement("th")
