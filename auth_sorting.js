191557269397c9f8e7552f91868771e32ca1260d
Hazem Ammar <45862155+Hazem-A@users.noreply.github.com>
2022-06-21 15:15:07 -0600

    Add files via upload

---
+++ auth_sorting.js
@@ -0,0 +1,44 @@
+let heading = document.querySelectorAll("body > main > div.l-report_grid > article > div > div.table-container > div > table > thead > tr")[0]
+
+// add three new categories to sort the problems by
+
+let auth_tot = document.querySelectorAll("body > main > div.l-report_grid > article > div > div.table-container > div > table > thead > tr > th:nth-child(8)")[0]
+let auth_tot_a = document.createElement("a")
+auth_tot_a.innerHTML="Authors"
+// check to see if the tab is already sorting
+if (window.location.href.includes("order=-authtot")){
+    auth_tot_a.href="?order=%2Bauthtot"
+} else {
+    auth_tot_a.href="?order=-authtot"
+}
+
+auth_tot.appendChild(auth_tot_a)
+
+
+let auth_acc = document.querySelectorAll("body > main > div.l-report_grid > article > div > div.table-container > div > table > thead > tr > th:nth-child(9)")[0]
+let auth_acc_a = document.createElement("a")
+auth_acc_a.innerHTML="Auth Acc."
+// check to see if the tab is already sorting
+if (window.location.href.includes("order=-authacc")){
+    auth_acc_a.href="?order=%2Bauthacc"
+} else {
+    auth_acc_a.href="?order=-authacc"
+}
+auth_acc.appendChild(auth_acc_a)
+
+
+
+let auth_rat = document.createElement("th")
+auth_rat.className="table-item-autofit"
+let auth_rat_a = document.createElement("a")
+auth_rat_a.innerHTML="Auth Rat."
+// check to see if the tab is already sorting
+if (window.location.href.includes("order=-authrat")){
+    auth_rat_a.href="?order=%2Bauthrat"
+} else {
+    auth_rat_a.href="?order=-authrat"
+}
+auth_rat.appendChild(auth_rat_a)
+heading.appendChild(auth_rat)
+
+
