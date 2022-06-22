41a87e91b848631a793389479b00f1c446ec3675
Hazem Ammar <45862155+Hazem-A@users.noreply.github.com>
2022-06-21 21:28:05 -0600

    Add files via upload

--- auth_sorting.js
+++ auth_sorting.js
@@ -1,57 +1,54 @@
+const sort = document.createElement("div");
 
-let sort = document.createElement("div")
+sort.className="strip-item-secondary";
 
-sort.className="strip-item-secondary"
+const name = document.createElement("strong");
+name.className = "contest-filter-head";
 
-let name = document.createElement("strong")
-name.className = "contest-filter-head"
+name.innerHTML = "Sort By:";
 
-name.innerHTML = "Sort By:"
+sort.appendChild(name);
 
-sort.appendChild(name)
-
-let auth_tot = document.createElement("div")
-let auth_tot_a = document.createElement("a")
-auth_tot_a.innerHTML="Authors"
-auth_tot.className="extra-button"
-    if (window.location.href.includes("order=-authtot")){
-        auth_tot_a.href="?order=%2Bauthtot"
-    } else {
-        auth_tot_a.href="?order=-authtot"
-    }
-auth_tot.appendChild(auth_tot_a)
-sort.appendChild(auth_tot)
-
+const auth_tot = document.createElement("div");
+const auth_tot_a = document.createElement("a");
+auth_tot_a.innerHTML="Authors";
+auth_tot.className="extra-button";
+if (window.location.href.includes("order=-authtot")){
+    auth_tot_a.href="?order=%2Bauthtot";
+} else {
+    auth_tot_a.href="?order=-authtot";
+}
+auth_tot.appendChild(auth_tot_a);
+sort.appendChild(auth_tot);
 
 
-let auth_acc = document.createElement("div")
-let auth_acc_a = document.createElement("a")
-auth_acc_a.innerHTML="Auth Acc."
-auth_acc.className="extra-button"
+
+const auth_acc = document.createElement("div");
+const auth_acc_a = document.createElement("a");
+auth_acc_a.innerHTML="Auth Acc.";
+auth_acc.className="extra-button";
 if (window.location.href.includes("order=-authacc")){
-    auth_acc_a.href="?order=%2Bauthacc"
+    auth_acc_a.href="?order=%2Bauthacc";
 } else {
-    auth_acc_a.href="?order=-authacc"
+    auth_acc_a.href="?order=-authacc";
 }
-auth_acc.appendChild(auth_acc_a)
-sort.appendChild(auth_acc)
+auth_acc.appendChild(auth_acc_a);
+sort.appendChild(auth_acc);
 
 
 
-let auth_rat = document.createElement("div")
-let auth_rat_a = document.createElement("a")
-auth_rat_a.innerHTML="Auth Rat."
-auth_rat.className="extra-button"
+const auth_rat = document.createElement("div");
+const auth_rat_a = document.createElement("a");
+auth_rat_a.innerHTML="Auth Rat.";
+auth_rat.className="extra-button";
 if (window.location.href.includes("order=-authrat")){
-    auth_rat_a.href="?order=%2Bauthrat"
+    auth_rat_a.href="?order=%2Bauthrat";
 } else {
-    auth_rat_a.href="?order=-authrat"
+    auth_rat_a.href="?order=-authrat";
 }
-auth_rat.appendChild(auth_rat_a)
-sort.appendChild(auth_rat)
+auth_rat.appendChild(auth_rat_a);
+sort.appendChild(auth_rat);
 
-let block = document.querySelector("body > main > div.l-report_grid > div")
-block.appendChild(sort)
-//create
-
+const block = document.querySelector("body > main > div.l-report_grid > div");
+block.appendChild(sort);
 
