84b25154881de52a6347228c107b453aa2122337
Hazem Ammar <45862155+Hazem-A@users.noreply.github.com>
2022-06-21 20:21:42 -0600

    Add files via upload

--- auth_sorting.js
+++ auth_sorting.js
@@ -1,72 +1,57 @@
-let heading = document.querySelectorAll("body > main > div.l-report_grid > article > div > div.table-container > div > table > thead > tr")[0]
 
-// add three new categories to sort the problems by
+let sort = document.createElement("div")
 
+sort.className="strip-item-secondary"
 
-// check to see if the user is logged in
-let logged_in = document.querySelector("body > main > div.l-report_grid > article > div > div.table-container > div > table > thead > tr > th:nth-child(2) > a")
-if (logged_in.innerHTML==="Solved"){
-    let auth_tot = document.querySelector("body > main > div.l-report_grid > article > div > div.table-container > div > table > thead > tr > th:nth-child(8)")
-    let auth_tot_a = document.createElement("a")
-    auth_tot_a.innerHTML="Authors"
-// check to see if the tab is already sorting
+let name = document.createElement("strong")
+name.className = "contest-filter-head"
+
+name.innerHTML = "Sort By:"
+
+sort.appendChild(name)
+
+let auth_tot = document.createElement("div")
+let auth_tot_a = document.createElement("a")
+auth_tot_a.innerHTML="Authors"
+auth_tot.className="extra-button"
     if (window.location.href.includes("order=-authtot")){
         auth_tot_a.href="?order=%2Bauthtot"
     } else {
         auth_tot_a.href="?order=-authtot"
     }
+auth_tot.appendChild(auth_tot_a)
+sort.appendChild(auth_tot)
 
-    auth_tot.appendChild(auth_tot_a)
 
 
-    let auth_acc = document.querySelector("body > main > div.l-report_grid > article > div > div.table-container > div > table > thead > tr > th:nth-child(9)")
-    let auth_acc_a = document.createElement("a")
-    auth_acc_a.innerHTML="Auth Acc."
-// check to see if the tab is already sorting
-    if (window.location.href.includes("order=-authacc")){
-        auth_acc_a.href="?order=%2Bauthacc"
-    } else {
-        auth_acc_a.href="?order=-authacc"
-    }
-    auth_acc.appendChild(auth_acc_a)
+let auth_acc = document.createElement("div")
+let auth_acc_a = document.createElement("a")
+auth_acc_a.innerHTML="Auth Acc."
+auth_acc.className="extra-button"
+if (window.location.href.includes("order=-authacc")){
+    auth_acc_a.href="?order=%2Bauthacc"
 } else {
-    let auth_tot = document.querySelector("body > main > div.l-report_grid > article > div > div.table-container > div > table > thead > tr > th:nth-child(7)")
-    let auth_tot_a = document.createElement("a")
-    auth_tot_a.innerHTML="Authors"
-// check to see if the tab is already sorting
-    if (window.location.href.includes("order=-authtot")){
-        auth_tot_a.href="?order=%2Bauthtot"
-    } else {
-        auth_tot_a.href="?order=-authtot"
-    }
-
-    auth_tot.appendChild(auth_tot_a)
-
-
-    let auth_acc = document.querySelector("body > main > div.l-report_grid > article > div > div.table-container > div > table > thead > tr > th:nth-child(8)")
-    let auth_acc_a = document.createElement("a")
-    auth_acc_a.innerHTML="Auth Acc."
-// check to see if the tab is already sorting
-    if (window.location.href.includes("order=-authacc")){
-        auth_acc_a.href="?order=%2Bauthacc"
-    } else {
-        auth_acc_a.href="?order=-authacc"
-    }
-    auth_acc.appendChild(auth_acc_a)
+    auth_acc_a.href="?order=-authacc"
 }
+auth_acc.appendChild(auth_acc_a)
+sort.appendChild(auth_acc)
 
 
-let auth_rat = document.createElement("th")
-auth_rat.className="table-item-autofit"
+
+let auth_rat = document.createElement("div")
 let auth_rat_a = document.createElement("a")
 auth_rat_a.innerHTML="Auth Rat."
-// check to see if the tab is already sorting
+auth_rat.className="extra-button"
 if (window.location.href.includes("order=-authrat")){
     auth_rat_a.href="?order=%2Bauthrat"
 } else {
     auth_rat_a.href="?order=-authrat"
 }
 auth_rat.appendChild(auth_rat_a)
-heading.appendChild(auth_rat)
+sort.appendChild(auth_rat)
+
+let block = document.querySelector("body > main > div.l-report_grid > div")
+block.appendChild(sort)
+//create
 
 
